6d155edf9bd53a45388f1f370378dd1e4d6e3bb7
Jessica Marques <jessica.marques.dev@gmail.com>
2021-05-03 17:19:05 -0300

    conf axios e utilizando prefetch na aplicação

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import NextLink from "next/link";
 import {
   Box,
   Button,
@@ -6,6 +6,7 @@ import {
   Flex,
   Heading,
   Icon,
+  Link,
   Table,
   Text,
   Tr,
@@ -17,39 +18,37 @@ import {
   useBreakpointValue,
 } from "@chakra-ui/react";
 import { RiAddLine } from "react-icons/ri";
-import { useQuery } from "react-query";
+
+import { getUsers, useUsers } from "../../services/hooks/useUsers";
 
 import { Sidebar } from "../../components/Sidebar";
 import { Header } from "../../components/Header";
 import Pagination from "../../components/Pagination";
+import { useState } from "react";
+import { queryClient } from "../../services/queryClient";
+import { api } from "../../services/api";
+import { GetServerSideProps } from "next";
 
-export default function UserList() {
-  const { data, isLoading, error } = useQuery("users", async () => {
-    const response = await fetch("http://localhost:3000/api/users");
-    const data = await response.json();
-
-    const users = data.users.map( user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year:'numeric'
-        })
-      };
-    });
-
-    return users;
-  }, {
-    staleTime: 1000 * 5 //5 seconds: ela não precisar ser recarregada por esse período.
+export default function UserList({ users }) {
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isFetching, error } = useUsers(page, {
+    initialData: users,
   });
 
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
+
+  async function handlePrefetchUser(userId: string) {
+    await queryClient.prefetchQuery(['user', userId], async ()=>{
+      const response = await api.get(`users/${userId}`)
+    
+      return response.data;
+    }, {
+      staleTime: 1000 * 60 * 10 //10 minutos
+    })
+  }
 
   return (
     <Box>
@@ -61,8 +60,11 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" color="gray.500" ml="4" />
+              )}
             </Heading>
-            <Link href="/users/create" passHref>
+            <NextLink href="/users/create" passHref>
               <Button
                 as="a"
                 size="sm"
@@ -72,7 +74,7 @@ export default function UserList() {
               >
                 Criar novo
               </Button>
-            </Link>
+            </NextLink>
           </Flex>
 
           {isLoading ? (
@@ -93,11 +95,10 @@ export default function UserList() {
                     </Th>
                     <Th>Usuário</Th>
                     {isWideVersion && <Th>Data de Cadastro</Th>}
-                    {/* <Th width="8"></Th> */}
                   </Tr>
                 </Thead>
                 <Tbody>
-                  {data.map((user) => {
+                  {data.users.map((user) => {
                     return (
                       <Tr key={user.id}>
                         <Td px={["4", "4", "6"]}>
@@ -105,7 +106,9 @@ export default function UserList() {
                         </Td>
                         <Td>
                           <Box>
-                            <Text fontWeight="bold">{user.name}</Text>
+                            <Link color="purple.400" onMouseEnter={()=> handlePrefetchUser(user.id)}>
+                              <Text fontWeight="bold">{user.name}</Text>
+                            </Link>
                             <Text fontSize="sm" color="grey.300">
                               {user.email}
                             </Text>
@@ -118,11 +121,25 @@ export default function UserList() {
                 </Tbody>
               </Table>
 
-              <Pagination />
+              <Pagination
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
+              />
             </>
           )}
         </Box>
       </Flex>
     </Box>
   );
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { users, totalCount } = await getUsers(1)
+
+  return {
+    props: {
+      users,
+    }
+  }
 }
